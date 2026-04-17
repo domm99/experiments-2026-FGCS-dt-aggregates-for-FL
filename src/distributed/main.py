1a514a921759971a4fe325280d48c80bae2230ec
davidedomini <davide.domini@unibo.it>
2026-04-17 19:43:01 +0200

    feat: add scheduling of training and inferences for experiment TrainAfterTime

--- src/distributed/main.py
+++ src/distributed/main.py
@@ -13,10 +13,11 @@ def load_patients(data_folder: str) -> tuple[list[dict], pd.Timestamp, pd.Timest
         df['timestamp'] = pd.to_datetime(
             df['Measurement_date'] + ' ' + df['Measurement_time']
         )
+        patient_id = df['Patient_ID'].iloc[0]
         ts = df['timestamp']
         min_time, max_time = ts.min(), ts.max()
-        patients.append({'data': df, 'min_time': min_time, 'max_time': max_time})
-        if min_time is None and max_time is None:
+        patients.append({'data': df, 'patient_id': patient_id, 'min_time': min_time, 'max_time': max_time})
+        if global_min is None and global_max is None:
             global_min = min_time
             global_max = max_time
         else:
@@ -26,31 +27,59 @@ def load_patients(data_folder: str) -> tuple[list[dict], pd.Timestamp, pd.Timest
                 global_max = max_time
     return patients, global_min, global_max
 
+def schedule_trainings(experiment, simulator, min_time):
+    if experiment == 'RetrainAfterTime':
+        for i in range(1, 4):
+            train_event = Event(
+                time=min_time + pd.DateOffset(years=i),
+                priority=2,
+                event_type='TRAIN',
+                payload={},
+            )
+            simulator.schedule_event(train_event)
+            test_event = Event(
+                time=min_time + pd.DateOffset(years=i) - pd.DateOffset(days=1),
+                priority=2,
+                event_type='INFERENCE',
+                payload={},
+            )
+            simulator.schedule_event(test_event)
+
 if __name__ == "__main__":
 
     config = LearningConfig()
     data_folder = 'T1DiabetesGranada/split'
     seeds = [0]
+    experiments = ['RetrainAfterTime'] # TODO add all the experiments
 
-    for seed in seeds:
-        seed_everything(seed)
+    for experiment in experiments:
+        for seed in seeds:
 
-        all_patients, min_time, max_time = load_patients(data_folder)
-        simulator = Simulator(data_folder, min_time, config, seed)
+            seed_everything(seed)
+            all_patients, min_time, max_time = load_patients(data_folder)
 
-        for patient in all_patients:
-            event_active = Event(
-                time = patient['min_time'],
-                priority = 0,
-                event_type = 'PATIENT_BECOMES_ACTIVE',
-                payload = patient,
-            )
+            print(f'Found {len(all_patients)} patients')
+            print(f'Min: {min_time}, Max: {max_time}')
 
-            event_inactive = Event(
-                time=patient['max_time'],
-                priority=0,
-                event_type='PATIENT_BECOMES_INACTIVE',
-                payload=patient,
-            )
-            simulator.schedule_event(event_active)
-            simulator.schedule_event(event_inactive)
+            simulator = Simulator(data_folder, min_time, max_time, config, seed)
+
+            # Schedule patients activation and deactivation
+            for patient in all_patients:
+                event_active = Event(
+                    time = patient['min_time'],
+                    priority = 0,
+                    event_type = 'PATIENT_BECOMES_ACTIVE',
+                    payload = patient,
+                )
+
+                event_inactive = Event(
+                    time=patient['max_time'],
+                    priority=0,
+                    event_type='PATIENT_BECOMES_INACTIVE',
+                    payload=patient,
+                )
+                simulator.schedule_event(event_active)
+                simulator.schedule_event(event_inactive)
+
+            # Schedule trainings and inferences
+            schedule_trainings(experiment, simulator, min_time)
