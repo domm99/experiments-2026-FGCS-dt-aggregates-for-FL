9c5fcc62987112ee025a7fa9b0ff2b215fac1bcf
davidedomini <davide.domini@unibo.it>
2026-04-17 18:37:08 +0200

    fix: fix model setter to load fresh model from new weights

--- src/distributed/DT.py
+++ src/distributed/DT.py
@@ -2,27 +2,22 @@ import pandas as pd
 from torch import nn
 from src.distributed.DTAggregate import DTAggregate
 from src.distributed.LearningConfig import LearningConfig
-from src.distributed.utils import load_patient_series, PatientSeries
+from src.distributed.utils import load_patient_series, PatientSeries, ForecastLSTM
 
 class DT:
 
     def __init__(self, mid: str, data_path: str, config: LearningConfig):
         self._mid = mid
+        self._time = None
         self._model = None
         self._config = config
+        self._is_active = False
         self._dt_aggregate = None
         self._data = pd.read_csv(f'{data_path}/{mid}.csv')
-        self._activation_date, self._stopping_date = self.__get_activation_interval()
 
-    def is_active(self, time: pd.Timestamp) -> bool:
-        return self._activation_date <= time <= self._stopping_date
-
-    def __get_activation_interval(self) -> tuple[pd.Timestamp, pd.Timestamp]:
-        self._data['timestamp'] = pd.to_datetime(
-            self._data['Measurement_date'] + ' ' + self._data['Measurement_time']
-        )
-        ts = self._data['timestamp']
-        return ts.min(), ts.max()
+    def activate(self, current_time: pd.Timestamp):
+        self._is_active = True
+        self._time = current_time
 
     @property
     def model(self):
@@ -30,7 +25,13 @@ class DT:
 
     @model.setter
     def model(self, model: nn.Module):
-        self._model = model
+        fresh_model = ForecastLSTM(
+            hidden_size=self._config.hidden_size,
+            num_layers=self._config.layers,
+            dropout=self._config.dropout,
+        )
+        fresh_model.load_state_dict(model.state_dict())
+        self._model = fresh_model
 
     @property
     def dt_aggregate(self):
